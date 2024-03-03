0bf87d4873a98c25652a5570156f2c35be347473
abdellah <abdellahedaoudi80@gmail.com>
2024-03-03 04:00:37 +0100

    add Another project 2**

--- app/Pages/Portfolio.jsx
+++ app/Pages/Portfolio.jsx
@@ -9,7 +9,42 @@ function Portfolio() {
       </div>
       {/* PORTFOLIO */}
       <div className="grid grid-cols-1  md:grid-cols-3  mx-3   justify-items-center  ">
-        {/* 1 */}
+        {/* 1  */}
+        <div className="bg-white h-[330px] w-80 rounded-lg shadow-lg pb-2 mb-5">
+          <img
+            className="w-96 rounded-md border-b-4 cursor-pointer"
+            src="Portfolio/EcommDigital.png"
+            alt="E-commerce App"
+          />
+          <div className="flex justify-center items-center">
+            <h1 className="p-3 underline">E-commerce App </h1>
+            <a
+              href="https://ecom-next-strapi.vercel.app"
+              className="flex items-center  pr-2"
+              target="_blank"
+            >
+              <img src="Portfolio/WebSite.png" width={20} alt="WebSite" />
+              <span className="ml-2 text-sm hover:text-blue-500">
+                Visit Website
+              </span>
+            </a>
+          </div>
+          <h2 className="px-3 text-[12px] pb-2  text-gray-500">
+            NextJs && Strapi and TailwindCSS App adaptable to all devices with
+            authentication and payment using stripe.
+          </h2>
+          <a
+            href="https://github.com/AbdellahEdaoudi/Ecom-NextStrapi.git"
+            className="flex items-center pr-2 float-end"
+            target="_blank"
+          >
+            <img src="Portfolio/github.png" width={24} alt="GitHub" />
+            <span className="ml-1 text-sm hover:text-blue-500">
+              View on GitHub
+            </span>
+          </a>
+        </div>
+        {/* 2 */}
         <div className="bg-white h-[330px] w-80 rounded-lg shadow-lg pb-2 mb-5">
           <img
             className="w-96 rounded-md border-b-4 cursor-pointer"
@@ -36,41 +71,6 @@ function Portfolio() {
           </h2>
           <a
             href="https://github.com/AbdellahEdaoudi/Ecommerce-ReactApp.git"
-            className="flex items-center pr-2 float-end"
-            target="_blank"
-          >
-            <img src="Portfolio/github.png" width={24} alt="GitHub" />
-            <span className="ml-1 text-sm hover:text-blue-500">
-              View on GitHub
-            </span>
-          </a>
-        </div>
-        {/* 2  */}
-        <div className="bg-white h-[330px] w-80 rounded-lg shadow-lg pb-2 mb-5">
-          <img
-            className="w-96 rounded-md border-b-4 cursor-pointer"
-            src="Portfolio/Ecom App.png"
-            alt="E-commerce App"
-          />
-          <div className="flex justify-center items-center">
-            <h1 className="p-3 underline">E-commerce App </h1>
-            <a
-              href="https://ecom-next-strapi.vercel.app"
-              className="flex items-center  pr-2"
-              target="_blank"
-            >
-              <img src="Portfolio/WebSite.png" width={20} alt="WebSite" />
-              <span className="ml-2 text-sm hover:text-blue-500">
-                Visit Website
-              </span>
-            </a>
-          </div>
-          <h2 className="px-3 text-[12px] pb-2  text-gray-500">
-            NextJs && Strapi and TailwindCSS App adaptable to all devices with
-            authentication and payment using stripe.
-          </h2>
-          <a
-            href="https://github.com/AbdellahEdaoudi/Ecom-NextStrapi.git"
             className="flex items-center pr-2 float-end"
             target="_blank"
           >
