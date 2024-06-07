7e824a3f7c277759bbd470371147abb884f58041
abdellah <abdellahedaoudi80@gmail.com>
2024-06-07 23:24:05 +0100

    Ads

--- app/layout.js
+++ app/layout.js
@@ -7,12 +7,18 @@ const prompt = Prompt({ subsets: ["latin"], weight: '500' });
 
 export const metadata = {
   title: "Abdellah Edaoudi",
-  description: "Portfolio Abdellah Edaoudi",
+  description: "Portfolio of Abdellah Edaoudi, a FullStack MERN Developer with expertise in building dynamic and responsive web applications.",
+  keywords: "Abdellah Edaoudi, FullStack Developer, MERN Stack, Portfolio, Web Development, React, Node.js, MongoDB, Express.js, JavaScript, Frontend Developer, Backend Developer, Software Engineer, Web Applications",
+
 };
 
 export default function RootLayout({ children }) {
   return (
     <html className="scroll-smooth" lang="en">
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2614061557764113"
+     crossorigin="anonymous"></script>
+      </head>
       <body className={prompt.className}>
         {children}
       </body>
